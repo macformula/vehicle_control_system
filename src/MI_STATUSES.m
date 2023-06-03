ed7fdb24e190f1ae6eacafb931ff04082ed58a81
LinF_PDX <fulin200028@gmail.com>
2023-06-03 16:50:45 -0400

    Add PWM status LED in DI ring, add motor ready state in MI

--- src/MI_STATUSES.m
+++ src/MI_STATUSES.m
@@ -3,9 +3,10 @@ classdef MI_STATUSES < Simulink.IntEnumType
         UNKNOWN(0)
         STS_INIT(1)
         STARTUP(2)
-        RUNNING(3)
-        SHUTDOWN(4)
-        MI_STS_ERROR(5)
-        OFF(6)
+        READY(3)
+        RUNNING(4)
+        SHUTDOWN(5)
+        MI_STS_ERROR(6)
+        OFF(7)
     end
 end
