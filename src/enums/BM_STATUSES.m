255f5d7c2cab416fa1fcd7c60543a82c3223861f
LinF_PDX <fulin200028@gmail.com>
2024-04-14 00:28:41 -0400

    Update control system for hvil status WIP

--- src/enums/BM_STATUSES.m
+++ src/enums/BM_STATUSES.m
@@ -7,12 +7,6 @@ classdef BM_STATUSES < Simulink.IntEnumType
         INIT_PRECHARGE(4)
         PRECHARGE(5)
         BM_RUNNING(6)
-        ERR_PRECHARGE_CLOSED(7)
-        ERR_STARTUP(8)
-        ERR_INIT_PRECHARGE(9)
-        ERR_PRECHARGE(10)
-        ERR_RUNNING(11)
-        ERR_ALL_CLOSED(12)
-        ERR_HV_POSITIVE(13)
+        HVIL_INTERRUPT(7)
     end
 end
--- src/enums/DI_CMD.m
+++ src/enums/DI_CMD.m
@@ -1,8 +1,9 @@
 classdef DI_CMD < Simulink.IntEnumType
     enumeration 
         DI_CMD_INIT(0)
-        READY_TO_DRIVE(1)
-        SYSTEM_ERROR(2)
-        DI_ERR_RESET(3)
+        HV_ON(1)
+        READY_TO_DRIVE(2)
+        SYSTEM_ERROR(3)
+        DI_ERR_RESET(4)
     end
 end
